7f7e62c5476f4ffe86d6355fb72369d79cd4ec5c
Guilherme <guilhermeshadow91@gmail.com>
2024-02-01 18:40:28 -0300

    feat: TODO wallet repository

--- src/repositories/prisma/PrismaRepository.ts
+++ src/repositories/prisma/PrismaRepository.ts
@@ -2,6 +2,8 @@ import { prisma } from '../../../prisma/client/client';
 import { User } from '../../../src/entities/user.entity';
 import { IUserRepository } from '../IUserRepositories';
 import { randomUUID } from 'crypto';
+import { IWalletRepository } from '../IWalletRepositories';
+import { Wallet } from 'src/entities/wallet.entity';
 
 class PrismaUsersRepository implements IUserRepository {
   async create({ name, lastName, email, password, cpf }): Promise<User> {
@@ -25,5 +27,12 @@ class PrismaUsersRepository implements IUserRepository {
     return users;
   }
 }
+
+//Ainda tenho que implementar o repository da carteira, não faço idéia de como vai seguir sinceramente(vou pensar)
+// class PrismaWalletRepository implements IWalletRepository {
+//   create({ balance }: Wallet): Promise<Wallet> {
+//     const wallet = await prisma.wallet.create
+//   }
+// }
 
 export { PrismaUsersRepository };
